73e10ac42cc6510a68a16384d0db760fc902bf9c
JunRain <sa2313@tukorea.ac.kr>
2024-12-28 17:48:34 +0900

    [greed/큰_수의_법칙]풀이 1

--- 큰_수의_법칙.py
+++ 큰_수의_법칙.py
@@ -1,16 +1,23 @@
 n, m, k = map(int, input().split())
 arr = list(map(int, input().split()))[:n]
 
-arr.sort(reverse=True)
+arr.sort()
+first = arr[n - 1]
+second = arr[n - 2]
 
 result = 0
-cnt = k
-for _ in range(m):
-    if cnt <= 0:
-        result += arr[1]
-        cnt = k
-    else:
-        cnt -= 1
-        result += arr[0]
+
+while True:
+    # k만큼 first를 반복해서 더한 후, second를 한 번 더함
+    # 위 행위를 while문에서 반복
+    for i in range(k):
+        if m == 0:
+            break;
+    result += first
+    m -= 1
+    if m == 0:
+        break
+    result += second
+    m -= 1
 
 print(result)
