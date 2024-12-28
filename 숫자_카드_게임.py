2ff01ceeb9569f31e3d8ace1818a080d1e884050
JunRain <sa2313@tukorea.ac.kr>
2024-12-28 19:14:38 +0900

    [greed/숫자_카드_게임] 풀이

--- 숫자_카드_게임.py
+++ 숫자_카드_게임.py
@@ -1,13 +1,9 @@
 n, m = map(int, input().split())
-card = list()
-
-for _ in range(n):
-    card.append(list(map(int, input().split()))[:m])
 
 max = 0
-
-for a in card:
-    min_value = min(a)
-    max = min_value if min_value > max else max
-
+for _ in range(n):
+    arr = list(map(int, input().split()))[:m]
+    min_value = min(arr)
+    max = max(max, min_value)
+    
 print(max)
