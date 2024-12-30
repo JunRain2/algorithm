8ae9f49f93184b2b94b051454c70cd92aa68ada7
JunRain <sa2313@tukorea.ac.kr>
2024-12-31 00:35:54 +0900

    [realization/게임_개발] 풀이

--- 게임_개발.py
+++ 게임_개발.py
@@ -1,36 +1,51 @@
 n, m = map(int, input().split())
 
-x, y, direction = map(int, input().split())
+d = [[0] * m for _ in range(n)]
 
-field = list()
-for _ in range(n):
-    field.append(list(map(int, input().split()))[:m])
+x, y, direction = map(int, input().split())
+d[x][y] = 1 # 현재 좌표 방문 처리
+
+array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
     
-steps = [(0, 1), (1,0), (0, -1), (-1, 0)] # 북, 동, 남, 서
-    
-    
-result = 0
-while True:
-    flag = True
-    for _ in range(4):
-        direction = (direction - 1) % 4 # 어째서인지 알아보기 / 문제에서 1번에 해당
-        tmp_x = x + steps[direction][0]
-        tmp_y = y + steps[direction][1]
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def turn_left():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
         
-        if field[tmp_y][tmp_x] == 0: # 문제에서 2번에 해당
-            field[x][y] = 1
-            result += 1
-            x = tmp_x
-            y = tmp_y
-            flag = False
+# 시뮬레이션 시작
+count = 1
+turn_time = 0
+while True:
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+    
+    else:
+        turn_time += 1
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        # 뒤로 갈 수 있다면 이동
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        # 뒤가 바다로 막혀있다면
+        else :
             break
-    
-    if flag: # 문제에서 3번에 해당
-        tmp_direction = (direction + 2) % 4
-        if field[y + steps[tmp_direction][1]][x + steps[tmp_direction][0]] == 1:
-            break
-        x += steps[tmp_direction][0]
-        y += steps[tmp_direction][1]
-    
-    
-print(result)
+        turn_time = 0
+
+print(count)
