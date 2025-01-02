fc1e99ba0ad54606728309b882a22424a46088cc
JunRain <sa2313@tukorea.ac.kr>
2025-01-02 16:00:49 +0900

    [search/떡볶이_떡_만들기] 풀이

--- 떡볶이_떡_만들기.py
+++ 떡볶이_떡_만들기.py
@@ -2,14 +2,25 @@ n, m = map(int, input().split())
 
 array = list(map(int, input().split()))[:n]
 
-value = max(array) - 1
+# 이진 탐색을 위한 시작점과 끝점 설정
+start = 0
+end = max(array)
 
-while value > 0:
-    a = list(filter(lambda x : x > value, array))
-    result = sum(a) - (len(a) * value)
-    
-    if result >= m:
-        print(value)
-        break;
+# 이진 탐색 수행(반복적)
+result = 0
+while start <= end:
+    total = 0
+    mid = (start + end) // 2
+    for x in array:
+        # 잘랐을 때의 떡의 양 계산
+        if x > mid:
+            total += x - mid
+    # 떡의 양이 부족한 경우 더 많이 자르기(왼쪽 부분 탐색)
+    if total < m:
+        end = mid - 1
+    # 떡의 양이 충분한 경우 덜 자르기(오른쪽 부분 탐색)
     else:
-        value -= 1
+        result = mid # 최대한 덜 잘랐을 때가 정답이므로, 여기에서 result 기록
+        start = mid + 1
+
+print(result)
