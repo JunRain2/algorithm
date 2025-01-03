f338e70f74841b70ce7f377279999fb0d13e2a81
JunRain <sa2313@tukorea.ac.kr>
2025-01-03 12:25:07 +0900

    [dp/효율적인_화폐_구성] 풀이

--- 효율적인_화폐_구성.py
+++ 효율적인_화폐_구성.py
@@ -1,18 +1,21 @@
 n, m = map(int, input().split())
-list = list()
 
-d = [0] * 10001
+array = list()
 
 for _ in range(n):
-    i = int(input())
-    list.append(i)
+    array.append(int(input()))
 
-for i in range(0, m + 1):
-    for j in list:
-        if d[i + j] == 0:
-            d[i + j] = d[i] + 1
-        else:
-            d[i + j] = min(d[i + j], d[i] + 1)
+# min()을 통해서 비교해야 하기 때문에 m의 최댓값을 넘어선 10001로 설정
+d = [10001] * (m + 1)
+d[0] = 0
 
-result = d[n] if d[n] == 0 else -1
-print(result)
+for i in range(n):
+    for j in range(array[i], m+1):
+        if d[j - array[i]] != 10001: #d[0] = 0이기 때문에 첫 번째 값이 패스될 일은 없다.
+            d[j] = min(d[j], d[j - array[i]] + 1)
+
+# 계산된 결과 출력
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
