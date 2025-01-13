c37804df97500fb3142d048c3c3a5158764d6c98
JunRain <sa2313@tukorea.ac.kr>
2025-01-13 21:28:56 +0900

    [sort/카드_정렬하기] 풀이

--- 카드_정렬하기.py
+++ 카드_정렬하기.py
@@ -1,13 +1,20 @@
+import heapq
+
 n = int(input())
-data = list()
+
+# 힙(Heap)에 초기 카드 묶음을 모두 삽입
+heap = []
 for _ in range(n):
-    data.append(int(input()))
+    data = int(input())
+    heapq.heappush(heap, data)
 
-total = 0
-for i in range(n - 1):
-    data.sort()
-    x, y = data.pop(0), data.pop(0)
-    total += x + y
-    data.append(x + y)
+result = 0
+# 힙(Heap)에 원소가 1개 남을 때까지
+while len(heap) != 1:
+    one, two = heapq.heappop(heap), heapq.heappop(heap)
+    # 카드 묶음을 합쳐서 다시 삽입
+    sum_value = one + two
+    result += sum_value
+    heapq.heappush(heap, sum_value)
 
-print(total)
+print(result)
