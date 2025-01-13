8cffce981a5148cc9365efd33d0cbed62a05b550
JunRain <sa2313@tukorea.ac.kr>
2025-01-13 17:25:04 +0900

    [sort/국영수] 풀이

--- 국영수.py
+++ 국영수.py
@@ -2,25 +2,9 @@ n = int(input())
 
 data = list()
 for _ in range(n):
-    name, a, b, c = input().split()
-    a, b, c = int(a), int(b), int(c)
-    data.append((a, b, c, name))
+    data.append(input().split())
 
-
-def sort_student(s1, s2):
-    if s1[0] != s2[0]:
-        return s1[0] - s2[0]  # 국어 내림차순
-    else:
-        if s1[1] != s2[1]:
-            return s2[1] - s1[1]  # 영어 오름차순
-        else:
-            if s1[2] != s2[2]:
-                return s1[2] - s2[2]  # 수학 내림차순
-            else:
-                return s2[3] - s1[3]  # 이름 오름차순
-
-
-data.sort(sort_student)
+data.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 
 for i in data:
-    print(i[3])
+    print(i[0])
