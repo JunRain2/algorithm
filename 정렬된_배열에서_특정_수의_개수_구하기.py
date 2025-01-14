f3ec8afc3b166a8c9f4f1c32c9183335b2317372
JunRain <sa2313@tukorea.ac.kr>
2025-01-14 13:49:03 +0900

    [search/정렬된_배열에서_특정_수의_개수_구하기] 내 풀이 루프

--- 정렬된_배열에서_특정_수의_개수_구하기.py
+++ 정렬된_배열에서_특정_수의_개수_구하기.py
@@ -1,14 +1,57 @@
-from bisect import bisect_right, bisect_left
+# 정렬된 수열에서 값이 x인 원소의 개수를 세는 메서드
+def count_by_value(array, x):
+    # 데이터의 개수
+    n = len(array)
+
+    # x가 처음 등장한 인덱스 계산
+    a = first(array, x, 0, n - 1)
+
+    # 수열에 x가 존재하지 않는 경우
+    if a == None:
+        return 0  # 값이 x인 원소의 개수는 0개이므로 0 반환
+
+    # x가 마지막으로 등장한 인덱스 계산
+    b = last(array, x, 0, n - 1)
+
+    # 개수를 반환
+    return b - a + 1
+
+
+# 처음 위치를 찾는 이진 탐색 메서드
+def first(array, target, start, end):
+    mid = (start + end) // 2
+    while end <= start:
+        if array[mid] == target:
+            while array[mid] == target:
+                mid -= 1
+            return mid + 1
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+
+def last(array, target, start, end):
+    mid = (start + end) // 2
+    while end <= start:
+        if array[mid] == target:
+            while array[mid] == target:
+                mid += 1
+            return mid - 1
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
 
 n, x = map(int, input().split())
-array = list(map(int, input().split()))[:n]
+array = list(map(int, input().split()))
 
-right_index = bisect_right(array, x)
-left_index = bisect_left(array, x)
+# 값이 x인 데이터의 개수 계산
+count = count_by_value(array, x)
 
-result = right_index - left_index
-
-if result == 0:
+if count == 0:
     print(-1)
-else :
-    print(result)
+else:
+    print(count)
