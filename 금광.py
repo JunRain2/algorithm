1ceff628538fa4e88349252b5da59c7a975370fc
JunRain <sa2313@tukorea.ac.kr>
2025-01-14 16:55:21 +0900

    [dp/금광] 풀이

--- 금광.py
+++ 금광.py
@@ -1,30 +1,35 @@
-t = int(input())
-
-for _ in range(t):
+# 테스트 케이스 입력
+for tc in range(int(input())):
+    # 금광 정보 입력
     n, m = map(int, input().split())
-    data = list(map(int, input().split()))
-    array = [[] * m for _ in range(n)]
+    array = list(map(int, input().split()))
     
-    for i in range(len(data)):
-        array[i // m][i % m] = data[i]
-    
-    d = [[0] * m for _ in range(n)] # 더해갈 값
+    # 다이나믹 프로그래밍을 위한 2차원 DP 테이블 초기화
+    dp = []
+    index = 0
     for i in range(n):
-        d[i][0] = array[i][0]
-    
-    dy = [-1, 0, 1]
-    for i in range(n):
-        for j in range(m - 1):
-            y, x = i, j
-            for k in range(3):
-                nx = x + 1
-                ny = y + dy[k]
-                if ny >= 0 and y < n:
-                    d[ny][nx] = max(d[y][x] + array[ny][nx], array[ny][nx])
-    
-    result = []            
-    for i in range(n):
-        result.append(d[i][len(m) - 1])
+        dp.append(array[index:index + m])
+        index += m
         
-    print(max(result))
+    # 다이나믹 프로그래밍 진행
+    for j in range(1, m):
+        for i in range(n):
+            # 왼쪽 위에서 오는 경우
+            if i == 0:
+                left_up = 0
+            else:
+                left_up = dp[i - 1][j - 1]
+            # 왼쪽 아래에서 오는 경우
+            if i == n - 1:
+                left_down = 0
+            else :
+                left_down = dp[i + 1][j - 1]
+            # 왼쪽에서 오는 경우
+            left = dp[i][j - 1]
+            dp[i][j] = dp[i][j] + max(left_up, left_down, left)
             
+    result = 0
+    for i in range(n):
+        result = max(result, dp[i][m - 1])
+    
+    print(result)
