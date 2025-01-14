2e17286a3872ee149588e0fa544e90ec96f9edaf
JunRain <sa2313@tukorea.ac.kr>
2025-01-14 14:19:39 +0900

    [search/고정점_찾기] 내 풀이

---
+++ 고정점_찾기.py
@@ -0,0 +1,18 @@
+n = int(input())
+array = list(map(int, input().split()))
+
+start = 0
+end = n
+
+result = -1
+while start <= end:
+    mid = (start + end) // 2
+    if array[mid] == mid:
+        result = mid
+        break
+    elif array[mid] > mid:
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(result)
