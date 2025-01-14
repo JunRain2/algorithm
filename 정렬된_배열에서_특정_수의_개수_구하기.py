172aa58ebcd708d3e6de9e0828bd5a1577c1ba39
JunRain <sa2313@tukorea.ac.kr>
2025-01-14 13:59:53 +0900

    [search/정렬된_배열에서_특정_수의_개수_구하기] 풀이 재귀

--- 정렬된_배열에서_특정_수의_개수_구하기.py
+++ 정렬된_배열에서_특정_수의_개수_구하기.py
@@ -19,31 +19,28 @@ def count_by_value(array, x):
 
 # 처음 위치를 찾는 이진 탐색 메서드
 def first(array, target, start, end):
-    result = -1
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] == target:
-            result = mid
-            end = mid - 1
-        elif array[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return result
-
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    # 해당 값을 가지는 원소 중에서 가장 왼쪽에 있는 경우에만 인덱스 반환
+    if (mid == 0 or target > array[mid - 1]) and array[mid] == target:
+        return mid
+    elif array[mid] >= target:
+        return first(array, target, start, mid -1)
+    else:
+        return first(array, target, mid + 1, end)
 
 def last(array, target, start, end):
-    result = -1
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] == target:
-            result = mid
-            start = mid + 1
-        elif array[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return result
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    # 해당 값을 가지는 원소 중에서 가장 오른쪽에 있는 경우에만 인덱스 반환
+    if (mid == n - 1 or target < array[mid + 1]) and array[mid] == target:
+        return mid
+    elif array[mid] >= target:
+        return last(array, target, start, mid -1)
+    else:
+        return last(array, target, mid + 1, end)
 
 n, x = map(int, input().split())
 array = list(map(int, input().split()))
