ce2ee158e450a3a62ce7f623c22519f62870409a
JunRain <sa2313@tukorea.ac.kr>
2025-01-14 13:56:04 +0900

    [search/정렬된_배열에서_특정_수의_개수_구하기] 풀이 루프

--- 정렬된_배열에서_특정_수의_개수_구하기.py
+++ 정렬된_배열에서_특정_수의_개수_구하기.py
@@ -7,7 +7,7 @@ def count_by_value(array, x):
     a = first(array, x, 0, n - 1)
 
     # 수열에 x가 존재하지 않는 경우
-    if a == None:
+    if a == -1:
         return 0  # 값이 x인 원소의 개수는 0개이므로 0 반환
 
     # x가 마지막으로 등장한 인덱스 계산
@@ -19,31 +19,31 @@ def count_by_value(array, x):
 
 # 처음 위치를 찾는 이진 탐색 메서드
 def first(array, target, start, end):
-    mid = (start + end) // 2
-    while end <= start:
+    result = -1
+    while start <= end:
+        mid = (start + end) // 2
         if array[mid] == target:
-            while array[mid] == target:
-                mid -= 1
-            return mid + 1
+            result = mid
+            end = mid - 1
         elif array[mid] > target:
             end = mid - 1
         else:
             start = mid + 1
-    return None
+    return result
 
 
 def last(array, target, start, end):
-    mid = (start + end) // 2
-    while end <= start:
+    result = -1
+    while start <= end:
+        mid = (start + end) // 2
         if array[mid] == target:
-            while array[mid] == target:
-                mid += 1
-            return mid - 1
+            result = mid
+            start = mid + 1
         elif array[mid] > target:
             end = mid - 1
         else:
             start = mid + 1
-    return None
+    return result
 
 n, x = map(int, input().split())
 array = list(map(int, input().split()))
