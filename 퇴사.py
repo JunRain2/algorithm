f270f4b2a6487894e8351f20795d1fb7b0c1a654
JunRain <sa2313@tukorea.ac.kr>
2025-01-14 18:24:08 +0900

    [dp/퇴사] 풀이

--- 퇴사.py
+++ 퇴사.py
@@ -1,17 +1,24 @@
 n = int(input())
-
-array = []
-for _ in range(n):
-    t, p = map(int, input().split())
-    array.append((t, p))
-
+t = []
+p = []
 dp = [0] * (n + 1)
+max_value = 0
 
-for i in range(n):
-    t, p = array[i]
-    day = i + t
-    if day <= n:
-        for j in range(day, n + 1):
-            dp[j] = max(dp[j], p + dp[i])
-
-print(max(dp))
+for _ in range(n):
+    x, y = map(int, input().split())
+    t.append(x)
+    p.append(y)
+    
+# 리스트를 뒤에서부터 거꾸로 확인
+for i in range(n -1 ,-1, -1):
+    time = t[i] + i
+    # 상담이 기간 안에 끝나는 경우
+    if time <= n:
+        # 점화식에 맞게, 현재까지 최고 이익 계산
+        dp[i] = max(p[i] + dp[time], max_value)
+        max_value = dp[i]
+    # 상담이 기간을 벗어나는 경우
+    else:
+        dp[i] = max_value
+        
+print(max_value)
