e96ef3e3c8390017e630e75bbeb90bdc28e23df4
JunRain <sa2313@tukorea.ac.kr>
2025-01-14 17:30:15 +0900

    [dp/정수_삼각형] 풀이

--- 정수_삼각형.py
+++ 정수_삼각형.py
@@ -1,20 +1,19 @@
 n = int(input())
 
-array = []
+dp = []
 for _ in range(n):
-    array.append(list(map(int, input().split())))
-    
-dp = [[0] * len(array[n-1]) for _ in range(n)]
-dp[0][0] = array[0][0]
+    dp.append(list(map(int, input().split())))
     
 for i in range(1, n):
-    for j in range(len(array[i])):
+    for j in range(i + 1):
         if j == 0:
             left = 0
         else:
             left = dp[i - 1][j - 1]
-            
-        right = dp[i - 1][j]
-        dp[i][j] = max(left, right) + array[i][j]
+        if j == i:
+            right = 0
+        else:
+            right = dp[i - 1][j]
+        dp[i][j] = max(left, right) + dp[i][j]
         
 print(max(dp[n - 1]))
