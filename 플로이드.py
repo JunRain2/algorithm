db04a4d75182f29a06c331d7bbc2c8914c7c974d
JunRain <sa2313@tukorea.ac.kr>
2025-01-15 15:49:09 +0900

    [shortest-path/플로이드] 풀이

--- 플로이드.py
+++ 플로이드.py
@@ -12,7 +12,7 @@ for i in range(1, n + 1):
 
 for _ in range(m):
     a, b, c = map(int, input().split())
-    graph[a][b] = c
+    graph[a][b] = min(graph[a][b], c)
 
 for k in range(1, n + 1):
     for a in range(1, n + 1):
