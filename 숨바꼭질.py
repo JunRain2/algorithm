9e3cc3f877d439e06eb2e31d2960b99f86cd599c
JunRain <sa2313@tukorea.ac.kr>
2025-01-15 17:48:18 +0900

    [shortest-path/숨바꼭질] 풀이

--- 숨바꼭질.py
+++ 숨바꼭질.py
@@ -26,9 +26,19 @@ while q:
             distance[i[0]] = cost
             heapq.heappush(q, (cost, i[0]))
 
+# 최단 거리가 가장 먼 노드 번호
+max_node = 0
+# 도달할 수 있는 노드 중에서, 최단 거리가 가장 먼 노드와의 최단 거리
+max_distance = 0
+# 최단 거리가 가장 먼 노드와의 최단 거리와 동일한 최단 거리를 가지는 노드들의 리스트
+result = []
 
-distance = [0 if x == INF else x for x in distance] # 리스트에서 특정 값만 변경
-max_distance = max(distance)
-indeces = [i for i, x in enumerate(distance) if x == max_distance] # 리스트에서 특정 값을 갖는 인덱스를 반환
-
-print(indeces[0], max_distance, len(indeces))
+for i in range(1, n + 1):
+    if max_distance < distance[i]:
+        max_node = i
+        max_distance = distance[i]
+        result = [max_node]
+    elif max_distance == distance[i]:
+        result.append(i)
+        
+print(max_node, max_distance, len(result))
