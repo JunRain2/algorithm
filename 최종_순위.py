caa2649b3e0e03123f000aeee0ad82711ff21340
JunRain <sa2313@tukorea.ac.kr>
2025-01-16 15:56:58 +0900

    [graph/최종_순위] 풀이

--- 최종_순위.py
+++ 최종_순위.py
@@ -1,39 +1,70 @@
 from collections import deque
 
 for tc in range(int(input())):
+    # 노드의 개수 입력 받기
     n = int(input())
-    graph = [[] for _ in range(n + 1)]
     indegree = [0] * (n + 1)
-
-    grades = list(map(int, input().split()))
-    for i in range(len(grades) - 1):
-        x, y = grades[i], grades[i + 1]
-        graph[x].append(y)
-        indegree[y] += 1
-
+    # 각 노드에 연결된 간선 정보를 담기 위한 인접 행렬 초기화
+    graph = [[False] * (n + 1) for _ in range(n + 1)]
+    # 작년 순위 정보 입력
+    data = list(map(int, input().split()))
+    # 방향 그래프의 간선 정보 초기화
+    for i in range(n):
+        for j in range(i + 1, n):
+            graph[data[i]][data[j]] = True
+            indegree[data[j]] += 1
+            
     m = int(input())
-    for _ in range(m):
+    for i in range(m):
         a, b = map(int, input().split())
-        graph[a].append(b)
-        indegree[b] += 1
-
-    # 위상 알고리즘
-    q = deque()
+        # 간선의 방향 뒤집기
+        if graph[a][b]:
+            graph[a][b] = False
+            graph[b][a] = True
+            indegree[a] += 1
+            indegree[b] -= 1
+        else:
+            graph[a][b] = True
+            graph[b][a] = False
+            indegree[a] -= 1
+            indegree[b] += 1
+            
     result = []
+    q = deque()
+    
     for i in range(1, n + 1):
         if indegree[i] == 0:
             q.append(i)
-
-    while q:
+            
+    certain = True # 위상 정렬 결과가 오직 하나인지 여부
+    cycle = False # 그래프 내 사이클이 존재하는지 여부
+    
+    # 정확히 노드의 개수만큼 반복
+    for i in range(n):
+        # 큐가 비어 있다면 사이클이 발생했다는 의미
+        if len(q) == 0:
+            cycle = True
+            break
+        # 큐의 원소가 2개 이상이라면 가능한 정렬 결과가 여러 개라는 의미
+        if len(q) >= 2:
+            certain = False
+            break
+        # 큐에서 원소 꺼내기
         now = q.popleft()
         result.append(now)
-        for i in graph[now]:
-            indegree[i] -= 1
-            if indegree[i] == 0:
-                q.append(i)
-
-    if len(result) == n:
-        for i in result:
-            print(i, end=" ")
-    else:
+        for j in range(1, n + 1):
+            if graph[now][j]:
+                indegree[j] -= 1
+                # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
+                if indegree[j] == 0:
+                    q.append(j)
+    # 사이클이 발생하는 경우
+    if cycle:
         print("IMPOSSIBLE")
+    # 위상 정렬 결과가 여러 개인 경우
+    elif not certain:
+        print("?")
+    else:
+        for i in result:
+            print(i, end = " ")
+        print()
