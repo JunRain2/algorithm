7c3f22716db66be82acc266a81e55745e2e5ea6b
JunRain <sa2313@tukorea.ac.kr>
2025-01-17 16:25:44 +0900

    [greed/순회강연] 내 풀이

---
+++ 순회강연.py
@@ -0,0 +1,40 @@
+# 탑승구 문제의 개념으로 풀이
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n = int(input())  # 대학의 수
+
+array = []
+
+max_day = 0
+for i in range(n):
+    p, d = map(int, input().split())  # 비용, ~일 안에
+    array.append((p, d))
+    max_day = max(max_day, d)
+
+parent = [0] * (max_day + 1)
+for i in range(1, max_day + 1):
+    parent[i] = i
+
+array.sort(reverse=True)
+cost = 0
+
+for p, d in array:
+    data = find_parent(parent, d)
+    if data != 0:
+        cost += p
+        union_parent(parent, d, d - 1)
+
+print(cost)
