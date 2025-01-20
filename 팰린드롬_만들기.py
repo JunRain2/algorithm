7258d0012490fccb1e3a4d6ce768a6b28f7dc2d3
JunRain <sa2313@tukorea.ac.kr>
2025-01-20 17:48:15 +0900

    [팰린드롬_만들기] 풀이

--- 팰린드롬_만들기.py
+++ 팰린드롬_만들기.py
@@ -1,33 +1,29 @@
 from collections import Counter
 
-array = list(input())
-if len(array) <= 1:
+# 입력 받기
+name = input().strip()
+
+# 각 문자의 등장 횟수 세기
+count = Counter(name)
+
+# 홀수 개수인 문자 찾기
+odd = 0
+odd_char = ''
+for char, freq in count.items():
+    if freq % 2 == 1:
+        odd += 1
+        odd_char = char
+
+# 팰린드롬 불가능한 경우
+if odd > 1:
     print("I'm Sorry Hansoo")
-    exit()
-counter = Counter(array)
-array_counter = list(counter.most_common())
-array_counter.sort()
-
-result = []
-odd_word = ""
-if len(array) % 2 == 0:  # 짝수 일 떄
-    for word, cnt in array_counter:
-        if cnt % 2 != 0:
-            print("I'm Sorry Hansoo")
-            exit()
-        for _ in range((cnt // 2)):
-            result.append(word)
-else:  # 홀수 일 떄
-    odd = 0
-    for word, cnt in array_counter:
-        if cnt % 2 != 0:
-            if odd > 0:
-                print("I'm Sorry Hansoo")
-                exit()
-            else:
-                odd_word = word
-                odd += 1
-        for _ in range((cnt // 2)):
-            result.append(word)
-
-print("".join(result) + "".join(odd_word) + "".join(result[::-1]))
+else:
+    # 팰린드롬 만들기
+    result = ''
+    # 사전순으로 앞쪽 절반 만들기
+    for char in sorted(count.keys()):
+        result += char * (count[char] // 2)
+    
+    # 중간에 홀수 문자 넣기
+    answer = result + odd_char + result[::-1]
+    print(answer)
