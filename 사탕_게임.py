eccb42456639326266692568217167b1588dd242
JunRain <sa2313@tukorea.ac.kr>
2025-01-20 14:42:21 +0900

    [realization/사탕_게임] 풀이

--- 사탕_게임.py
+++ 사탕_게임.py
@@ -7,34 +7,45 @@ for _ in range(n):
 
 # 빨간색은 C, 파란색은 P, 초록색은 Z, 노란색은 Y
 def max_length(array):
-    result = 0
+    max_cnt = 1
+    
+    # 가로 방향 체크
     for i in range(n):
-        for j in range(n):
-            x_cnt = 1
-            for x in range(i + 1, n):
-                if array[i][j] == array[x][j]:
-                    x_cnt += 1
-            y_cnt = 1
-            for y in range(j + 1, n):
-                if array[i][j] == array[i][y]:
-                    y_cnt += 1
-            result = max(result, x_cnt, y_cnt)
-    return result
+        cnt = 1
+        for j in range(1, n):
+            if array[i][j] == array[i][j-1]:
+                cnt += 1
+            else:
+                cnt = 1
+            max_cnt = max(max_cnt, cnt)
+    
+    # 세로 방향 체크
+    for j in range(n):
+        cnt = 1
+        for i in range(1, n):
+            if array[i][j] == array[i-1][j]:
+                cnt += 1
+            else:
+                cnt = 1
+            max_cnt = max(max_cnt, cnt)
+    
+    return max_cnt
 
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+
+dx = [1, 0]
+dy = [0, 1]
 
 result = 0
 for x in range(n):
     for y in range(n):
-        for i in range(4):
+        for i in range(2):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<= nx < n and 0 <= ny < n:
+            if 0 <= nx < n and 0 <= ny < n and array[x][y] != array[nx][ny]:
                 array[x][y], array[nx][ny] = array[nx][ny], array[x][y]
                 result = max(result, max_length(array))
                 if result == n:
                     break
                 array[x][y], array[nx][ny] = array[nx][ny], array[x][y]
-                
+
 print(result)
