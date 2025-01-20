0cce11bf7a903d58983dc18accc106c5e00da1f7
JunRain <sa2313@tukorea.ac.kr>
2025-01-20 15:52:48 +0900

    [realization/참외밭] 풀이

--- 문법/참외밭.py
+++ 문법/참외밭.py
@@ -1,36 +1,30 @@
-k = int(input())  # 1m에 자라나는 참외의 개수
-
-array = []
-max_x = 0  # 가로 최고 길이
-max_y = 0  # 세로 최고 길이
-min_x = 0
-min_y = 0
+k = int(input())
+directions = []
+lengths = []
 
 for i in range(6):
-    a, b = map(int, input().split())
-    if a == 1 or a == 2:
-        max_x = max(b, max_x)
-    elif a == 3 or a == 4:
-        max_y = max(b, max_y)
-    if i == 2 and a == 4:
-        min_y = b
-        min_x = array[i - 1][1]
-    if i == 3 and a == 2:
-        min_x = b
-        min_y = array[i - 1][1]
-    if i == 4:
-        if a == 3:  # 남
-            min_y = b
-            min_x = array[i - 1][1]
-        if a == 1:
-            min_x = b
-            min_y = array[i - 1][1]
-    array.append((a, b))
+    direction, length = map(int, input().split())
+    directions.append(direction)
+    lengths.append(length)
 
-# 육각형을 데이터로 어떻게 표현할 것인가
-# 작은 사각형을 어떻게 찾을 것인가?
-max_square = max_x * max_y  # 사각형 전체 합
-min_square = min_x * min_y
+# 방향과 길이를 두 번 이어붙여서 패턴을 찾기 쉽게 함
+directions = directions + directions
+lengths = lengths + lengths
 
-result = k * (max_square - min_square)
+max_area = 0
+min_area = 0
+
+# 전체 육각형에서 가장 긴 가로, 세로 찾기
+width = max([lengths[i] for i in range(6) if directions[i] in [1, 2]])
+height = max([lengths[i] for i in range(6) if directions[i] in [3, 4]])
+max_area = width * height
+
+# 빈 사각형 찾기
+for i in range(6):
+    # 연속된 네 변에서 첫 번째와 세 번째, 두 번째와 네 번째가 각각 평행할 때
+    if directions[i] == directions[i+2] and directions[i+1] == directions[i+3]:
+        min_area = lengths[i+1] * lengths[i+2]
+        break
+
+result = (max_area - min_area) * k
 print(result)
