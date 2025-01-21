c2f9fbbf2844498cc741aa1d1456bf62c2d2c2b9
JunRain <sa2313@tukorea.ac.kr>
2025-01-21 14:08:30 +0900

    [병든_나이트] 풀이

--- 병든_나이트.py
+++ 병든_나이트.py
@@ -1,32 +1,9 @@
-from collections import deque
-
-dx = [-2, -1, 1, 2]
-dy = [1, 2, 2, 1]
-
-n, m = map(int, input().split())  # 세로 길이, 가로 길이
-
-flag = [False] * 4
-
-q = deque()
-q.append((n, 0))
-result = 0
-while q:
-    x, y = q.popleft()
-    result += 1
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m:
-            q.append((nx, ny))
-            flag[i] = True
-            
-if result > 4:
-    result_flag = True
-    for f in flag:
-        if not f:
-            result_flag = False
-
-if not result_flag:
-    print(4)
+n, m = map(int, input().split())
+if n == 1:
+    print(1)
+elif n == 2:
+    print(min(4, (m + 1) // 2))
+elif m <= 6:
+    print(min(4, m))
 else:
-    print(result)
+    print(m - 2)
