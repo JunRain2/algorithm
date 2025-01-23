1fe18aaec82df54d5e284cc28908b4c5a2c06805
JunRain <sa2313@tukorea.ac.kr>
2025-01-23 18:38:10 +0900

    [두_용액] 풀이

--- 두_용액.py
+++ 두_용액.py
@@ -6,7 +6,7 @@ result = [0, 0]
 start_idx = 0
 end_idx = n - 1
 gap = int(2*1e9)
-while start_idx <= end_idx and gap != 0:
+while start_idx < end_idx and gap != 0:
     start, end = array[start_idx], array[end_idx]
     if gap > abs(end + start):
         result = [start, end]
