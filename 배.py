8d254a16ea23c5e351aece12b0d380f70f71446c
JunRain <sa2313@tukorea.ac.kr>
2025-01-23 23:13:38 +0900

    [저울] 풀이

--- 배.py
+++ 배.py
@@ -1,30 +1,25 @@
-import heapq
-
 n = int(input())
-weights = list(map(int, input().split()))
+cranes = list(map(int, input().split()))
 m = int(input())
 boxes = list(map(int, input().split()))
 
-if max(weights) < max(boxes):
+if max(cranes) < max(boxes):
     print(-1)
     exit()
 
-boxes = [-i for i in boxes]
-
-weights.sort(reverse=True)
+# 크레인, 박스 모두 내림차순 정렬
+cranes.sort(reverse=True)
+boxes.sort(reverse=True)
 
 result = 0
+# 모든 박스가 옮겨질 때까지 반복
 while boxes:
-    tmp = []
-    for w in weights:
-        if not boxes:
-            break
-        tmp_box = -heapq.heappop(boxes)
-        while tmp_box > w and boxes:
-            tmp.append(-tmp_box)
-            tmp_box = -heapq.heappop(boxes)
-    for t in tmp:
-        heapq.heappush(boxes, t)
+    for crane in cranes:
+        # 현재 크레인으로 옮길 수 있는 가장 무거운 박스를 찾으면 바로 제거
+        for i in range(len(boxes)):
+            if boxes[i] <= crane:
+                boxes.pop(i)
+                break
     result += 1
-
+    
 print(result)
