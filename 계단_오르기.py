d8c094d5405d00bcccd7a0d2a159954144b57f3c
JunRain <sa2313@tukorea.ac.kr>
2025-01-27 17:03:10 +0900

    [계단_오르기] 풀이

--- 계단_오르기.py
+++ 계단_오르기.py
@@ -1,20 +1,15 @@
 n = int(input())
-array = []
-for _ in range(n):
-    array.append(int(input()))
+array = [0] * (n + 1)
+for i in range(1, n + 1):
+    array[i] = int(input())
 
 dp = [0] * (n + 1)
-dp[1] = array[0]
+dp[1] = array[1]
+if n >= 2:
+    dp[2] = dp[1] + array[2]
 
-cnt = -1
-for i in range(2, n + 1):
-    a, b = dp[i - 1], dp[i - 2]
-    if a <= b or cnt == 1:
-        cnt = 0
-        dp[i] = b + array[i - 1]
-    else:
-        cnt += 1
-        dp[i] = a + array[i - 1]
+for i in range(3, n + 1):
+    dp[i] = max(dp[i - 2], dp[i - 3] + array[i - 1]) + array[i]
 
 
 print(dp[n])
