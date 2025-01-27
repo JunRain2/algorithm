52a7bec6154037bf703436c1e04d4aa643441430
JunRain <sa2313@tukorea.ac.kr>
2025-01-27 20:06:39 +0900

    [연속_합] 풀이

--- 연속_합.py
+++ 연속_합.py
@@ -1,16 +1,7 @@
-from itertools import combinations
-
 n = int(input())
 array = list(map(int, input().split()))
 
-dp = []
-dp.append(array[0])
 for i in range(1, n):
-    s = dp[i - 1] + array[i]
-    dp.append(s)
-
-result =  -int(1e9)
-for a, b in list(combinations(dp, 2)):
-    result = max(result, b - a)
-
-print(result)
+    array[i] = max(array[i - 1] + array[i], array[i])
+    
+print(max(array))
