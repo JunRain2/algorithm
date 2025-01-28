5798e29df049a16ba65cbf95b0ce0a26abb1ad03
JunRain <sa2313@tukorea.ac.kr>
2025-01-28 19:12:26 +0900

    [인터넷_설치] 풀이

--- 인터넷_설치.py
+++ 인터넷_설치.py
@@ -3,41 +3,40 @@ import sys
 input = sys.stdin.readline
 INF = int(1e9)
 
-# 나머지 인터넷 선 중 제일 비싼 것
-n, p, k = map(int, input().split()) # 학생들 번호, 케이블 선 개수, 꽁자 제공 케이블 선
+def dijkstra(mid):
+    distance = [INF] * (n + 1)
+    q = [(0, 1)]
+    distance[1] = 0
+    
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for nxt, cost in graph[now]:
+            new_cost = dist + (cost > mid) # 비용이 더 크면 +1
+            if new_cost < distance[nxt]:
+                distance[nxt] = new_cost
+                heapq.heappush(q, (new_cost, nxt))
+    
+    return distance[n] <= k
 
+n, p, k = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
+
 for _ in range(p):
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
     graph[b].append((a, c))
-    
-parent = [() for _ in range(n + 1)]
-    
-distance = [INF] * (n + 1)
-q = []
-q.append((0, 1))
-distance[1] = 0
-while q:
-    dist, now = heapq.heappop(q)
-    if dist > distance[now]:
-        continue
-    for b, c in graph[now]:
-        cost = c + dist
-        if cost < distance[b]:
-            parent[b] = (now, c)
-            distance[b] = cost
-            heapq.heappush(q, (cost, b))
-            
-if distance[n] < INF:
-    x = n
-    result = []
-    while x != 1:
-        index, cost = parent[x]
-        x = index
-        result.append(cost)
-    result.sort(reverse=True)
-    print(result[k])
-    
-else:
-    print(-1)
+
+left, right = 0, 1000000
+result = -1
+
+while left <= right:
+    mid = (left + right) // 2
+    if dijkstra(mid):
+        result = mid
+        right = mid - 1
+    else:
+        left = mid + 1
+
+print(result)
