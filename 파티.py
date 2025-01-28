ba0749b1af37afb94c232a8de485bf900b965d3e
JunRain <sa2313@tukorea.ac.kr>
2025-01-28 15:37:12 +0900

    [파티] 내 풀이

--- 파티.py
+++ 파티.py
@@ -1,5 +1,4 @@
 import sys
-import heapq
 
 input = sys.stdin.readline
 
@@ -7,34 +6,23 @@ INF = int(1e9)
 
 n, m, x = map(int, input().split())
 
-graph = [[] for _ in range(n + 1)]
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    graph[i][i] = 0
 
 for _ in range(m):
     a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-
-
-def dijkstra(start, end):
-    distance = [INF] * (n + 1)
-    q = []
-    q.append((0, start))
-    distance[start] = 0
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-        for b, c in graph[now]:
-            cost = dist + c
-            if cost < distance[b]:
-                distance[b] = cost
-                heapq.heappush(q, (cost, b))
-    return distance[end]
+    graph[a][b] = c
+    
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
 
 result = 0
-for i in range(1, n + 1):
-    a = dijkstra(i, x)
-    b = dijkstra(x, i)
-    result = max(result, a + b)
+for i in range(1, n+1):
+    result = max(result, graph[i][x] + graph[x][i])
 
 print(result)
