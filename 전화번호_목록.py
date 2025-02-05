beee6c6357754e06ba82be2741c2c3b89e7d5369
JunRain <sa2313@tukorea.ac.kr>
2025-02-05 14:10:35 +0900

    [전화번호_목록] 풀이

--- 전화번호_목록.py
+++ 전화번호_목록.py
@@ -1,11 +1,6 @@
 def solution(phone_book):
-    answer = True
-    n = len(phone_book)
-    phone_book.sort() # 길이가 작은 수부터
-    
-    for i in range(n):
-        for j in range(i + 1, n):
-            if phone_book[j].startswith(phone_book[i]):
-                return False
-            
-    return answer
+    phone_book.sort()
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
