2e1952f6969dd97635fde83a931f4b42ce6f88dc
JunRain <sa2313@tukorea.ac.kr>
2025-02-05 14:55:48 +0900

    [의상] 풀이

--- 의상.py
+++ 의상.py
@@ -1,17 +1,12 @@
 from collections import Counter
 from itertools import combinations
 
-
 def solution(clothes):
-    answer = 0
     kind_counter = dict(Counter([x[1] for x in clothes]))
     clothes_cnt = kind_counter.values()
+    
+    answer = 1
+    for cnt in clothes_cnt:
+        answer *= (cnt + 1)
 
-    for i in range(1, len(clothes_cnt)):
-        for j in combinations(clothes_cnt, i):
-            tmp = 1
-            for k in j:
-                tmp *= k
-        answer += tmp
-
-    return answer
+    return answer - 1
