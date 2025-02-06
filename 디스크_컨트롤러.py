3edcb5a4c1f4c6f0e10a5bf1495dfa5e04aee4d6
JunRain <sa2313@tukorea.ac.kr>
2025-02-06 14:29:37 +0900

    [디스크_컨트롤러] 풀이

--- 디스크_컨트롤러.py
+++ 디스크_컨트롤러.py
@@ -3,32 +3,25 @@ import heapq
 
 def solution(jobs):
     answer = 0
-    jobs.sort()
-    q = []
-    current_time = 0
-    idx = 0
-    while True:
-        if idx >= len(jobs):
-            break
-        # 현재 스케줄러가 끝났을 때, 시간보다 작으면 큐에 입력
-        if current_time >= jobs[idx][0]:
-            s, l = jobs[idx]
-            heapq.heappush(q, (l, s, idx))
-            idx += 1
-        # 현재 스케줄러가 끝났을 때, 시간보다 크면
-        else:
-            while q:
-                l, s, i = heapq.heappop(q)
-                current_time += l
-                answer += current_time - s
-            s, l = jobs[idx]
-            heapq.heappush(q, (l, s, idx))
-            idx += 1
+    if not jobs:
+        return 0
 
-    # jobs의 값을 모두 q에 넣었을 경우 털어줘야 함
-    while q:
-        l, s, i = heapq.heappop(q)
-        current_time += l
-        answer += current_time - s
+    jobs.sort(key=lambda x: x[0])
+    current_time = 0
+    job_queue = []
+    job_index = 0
+
+    while job_index < len(jobs) or job_queue:
+        while job_index < len(jobs) and jobs[job_index][0] <= current_time:
+            start_time, length = jobs[job_index]
+            heapq.heappush(job_queue, (length, start_time))
+            job_index += 1
+
+        if job_queue:
+            length, start_time = heapq.heappop(job_queue)
+            current_time += length
+            answer += current_time - start_time
+        elif job_index < len(jobs):
+            current_time = jobs[job_index][0]
 
     return answer // len(jobs)
