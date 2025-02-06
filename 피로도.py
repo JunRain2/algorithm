e20897673d8a8cc23aab686fb3ae456cac14abfa
JunRain <sa2313@tukorea.ac.kr>
2025-02-06 19:45:42 +0900

    [피로도] 풀이

--- 피로도.py
+++ 피로도.py
@@ -1,21 +1,12 @@
-def dfs(k, v, visitd, dungeons, cnt):
-    if k < 0 or cnt == len(dungeons):
-        return cnt
-    max_cnt = cnt
+def dfs(k, dungeons, visited, count):
+    max_count = count
     for i in range(len(dungeons)):
-        if not visitd[i] or dungeons[i][0] <= k:
-            visitd[i] = True
-            max_cnt = max(dfs(k - dungeons[i][1], i, visitd, dungeons, cnt + 1), max_cnt)
-            visitd[i] = False 
-    return max_cnt
+        if not visited[i] and dungeons[i][0] <= k:
+            visited[i] = True
+            max_count = max(dfs(k - dungeons[i][1], dungeons, visited, count + 1), max_count)
+            visited[i] = False 
+    return max_count
 
 def solution(k, dungeons):
-    answer = -1
-    visitd = [False] * len(dungeons)
-    for i in range(len(dungeons)):
-        if dungeons[i][0] <= k:
-            visitd[i] = True
-            answer = max(dfs(k, i, visitd, dungeons, 0), answer)
-            visitd[i] = False
-        
-    return answer
+    visited = [False] * len(dungeons)
+    return dfs(k, dungeons, visited, 0)
