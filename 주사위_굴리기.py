a817115594a196cd7c0f2775d72225c2ea7d9d07
JunRain <sa2313@tukorea.ac.kr>
2025-02-11 23:53:57 +0900

    [주사위_굴리기] 풀이

--- 주사위_굴리기.py
+++ 주사위_굴리기.py
@@ -2,34 +2,43 @@ n, m, x, y, k = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 orders = list(map(int, input().split()))
 
-# 동, 서, 북, 남
+# 동, 서, 북, 남의 이동 방향 (order: 1,2,3,4)
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
 
-# 주사위를 굴렸을 때, 이동한 칸에 쓰여 있는 수가 0이면,주사위의 바닥면에 쓰여 있는 수가 복사
-# 0이 아닌 경우, 칸에 쓰여 있는 수가 주사위의 바닥면으로 복사, 칸에 쓰여있는 수는 0
-
+# 주사위 상태:
+# dice[1]: 윗면, dice[2]: 북쪽, dice[3]: 동쪽, dice[4]: 서쪽, dice[5]: 남쪽, dice[6]: 바닥면
 dice = [0] * 7
-current = 1
-# 동, 서, 북, 남, 반대
-dices = [
-    [],
-    [3, 4, 2, 5, 6],  # 1
-    [3, 4, 6, 1, 5],  # 2
-    [6, 1, 2, 5, 4],  # 3
-    [1, 6, 2, 5, 3],  # 4
-    [3, 4, 1, 6, 2],  # 5
-    [3, 4, 5, 2, 1],  # 6
-]
+
 for order in orders:
-    nx, ny = x + dx[order - 1], y + dy[order - 1]
+    nx = x + dx[order - 1]
+    ny = y + dy[order - 1]
+
+    # 격자 범위 검사 (세로 크기 n, 가로 크기 m)
     if nx < 0 or nx >= n or ny < 0 or ny >= m:
         continue
-    current = dices[current][order - 1]
+
+    # 이동한 방향에 따라 주사위의 면 재배치
+    if order == 1:  # 동쪽
+        dice[1], dice[4], dice[6], dice[3] = dice[4], dice[6], dice[3], dice[1]
+
+    elif order == 2:  # 서쪽
+        dice[1], dice[3], dice[6], dice[4] = dice[3], dice[6], dice[4], dice[1]
+
+    elif order == 3:  # 북쪽
+        dice[1], dice[5], dice[6], dice[2] = dice[5], dice[6], dice[2], dice[1]
+
+    elif order == 4:  # 남쪽
+        dice[1], dice[2], dice[6], dice[5] = dice[2], dice[6], dice[5], dice[1]
+
+    # 새로운 위치로 이동
     x, y = nx, ny
+
+    # 이동한 칸의 값에 따라 복사 작업 수행
     if graph[x][y] == 0:
-        graph = dice[dices[current][4]]
-    elif dice[dices[current][4]] == 0:
-        dice[dices[current][4]] = graph[x][y]
-    
-    print(dice[current])
+        graph[x][y] = dice[6]
+    else:
+        dice[6] = graph[x][y]
+        graph[x][y] = 0
+
+    print(dice[1])
