4e7380fc14a1024aeeb4753e537adc13faae1f52
JunRain <sa2313@tukorea.ac.kr>
2025-02-11 14:44:21 +0900

    [단속카메라] 내 풀이{오답 풀이}

--- 단속카메라.py
+++ 단속카메라.py
@@ -1,14 +1,14 @@
 # 진입, 진출
 def solution(routes):
-    routes.sort()    
+    routes.sort()
     answer = 1
-    
-    start = routes[0][0]
-    end = routes[0][1]
+
+    start, end = routes[0]
     
     for i in range(1, len(routes)):
         if start <= routes[i][0] <= end:
-            start = routes[i][0]
+            start = max(routes[i][0], start)
+            end = min(routes[i][1], end)
         else:
             answer += 1
             start = routes[i][0]
