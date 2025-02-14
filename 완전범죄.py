ffea8e35e903b31c9640c0172fac68b4359d1795
JunRain <sa2313@tukorea.ac.kr>
2025-02-14 15:14:41 +0900

    [완전범죄] 백트래킹 + dp

--- 완전범죄.py
+++ 완전범죄.py
@@ -5,14 +5,17 @@ INF = int(1e9)
 def solution(info, n, m):
     answer = 0
     
+    dp = [[INF] * 121 for _ in range(121)]
     def back_traking(a, b, i):
         if a >= n or b >= m:
             return INF
         if i == len(info):
             return a
-
         
-        return min(back_traking(a + info[i][0], b, i + 1), back_traking(a, b + info[i][1], i + 1))
+        if dp[a][b] >= INF:
+            dp[a][b] = min(back_traking(a + info[i][0], b, i + 1), 
+                            back_traking(a, b + info[i][1], i + 1))
+        return dp[a][b]
     
     answer = back_traking(0, 0, 0)
     if answer >= INF:
