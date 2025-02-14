4ba8f05976465d6eecadb8b1826bc32ba4f5aeb6
JunRain <sa2313@tukorea.ac.kr>
2025-02-14 15:23:01 +0900

    [완전범죄] 풀이

--- 완전범죄.py
+++ 완전범죄.py
@@ -2,23 +2,29 @@
 
 INF = int(1e9)
 
+
 def solution(info, n, m):
     answer = 0
-    
-    dp = [[INF] * 121 for _ in range(121)]
+
+    dp = {}  # (i, a, b)를 키로 하는 메모제이션(dict())
+
     def back_traking(a, b, i):
         if a >= n or b >= m:
             return INF
         if i == len(info):
             return a
-        
-        if dp[a][b] >= INF:
-            dp[a][b] = min(back_traking(a + info[i][0], b, i + 1), 
-                            back_traking(a, b + info[i][1], i + 1))
-        return dp[a][b]
-    
+        if (i, a, b) in dp:
+            return dp[(i, a, b)]
+
+        mv = min(
+            back_traking(a + info[i][0], b, i + 1),
+            back_traking(a, b + info[i][1], i + 1),
+        )
+        dp[(i, a, b)] = mv
+        return mv
+
     answer = back_traking(0, 0, 0)
     if answer >= INF:
         return -1
-        
+
     return answer
