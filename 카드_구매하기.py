ec235e79c201f5b793e8f9de9a21bde525194e7c
JunRain <sa2313@tukorea.ac.kr>
2025-02-15 17:00:16 +0900

    [카드_구매하기] dp

--- 카드_구매하기.py
+++ 카드_구매하기.py
@@ -2,23 +2,12 @@
 # 돈을 많이 써서 N개의 카드팩을 구매
 n = int(input())
 array = [0] + list(map(int, input().split()))
-dp = dict()
+dp = [0] * (n + 1)
 
-
-# 백트래킹 연산
-def dfs(pay, card):
-    if card == n:
-        return pay
-    if (pay, card) in dp.keys():
-        return dp[(pay, card)]
-    dfs_result = []
-    for i in range(1, n + 1):
-        if card + i > n:
-            continue
-        dfs_result.append(dfs(pay + array[i], card + i))
-    dp[(pay, card)] = max(dfs_result)
-
-    return dp[(pay, card)]
-
-
-print(dfs(0, 0))
+for i in range(1, n + 1):
+    tmp = [array[i]]
+    for j in range(1, i):
+        tmp.append(dp[i - j] + array[j])
+    dp[i] = max(tmp)
+    
+print(dp[n])
