b8f2b8876e4af12b1528b1808e0950772f03b6ce
JunRain <sa2313@tukorea.ac.kr>
2025-02-27 20:41:11 +0900

    [부분합] 풀이

--- 부분_합.py
+++ 부분_합.py
@@ -1,16 +1,22 @@
 n, m = map(int, input().split())
 data = list(map(int, input().split()))
 
-start, end = 0, 1
-result = n
-for i in range(n):
-    if sum(data[start:end]) < m:
+start, end = 0, 0
+total = 0
+result = n + 1
+
+while True:
+    if total < m:
+        if end == n:
+            break;
+        total += data[end]
         end += 1
     else:
-        while sum(data[start:end]) >= m:
-            result = min(result, end - start)
-            start += 1
-if result == n:
-    print(-1)
+        result = min(result, end - start)
+        total -= data[start]
+        start += 1
+        
+if result == (n + 1):
+    print(0)
 else:
     print(result)
