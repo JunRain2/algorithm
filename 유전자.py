e13838221518b256be378e09081bd71b8a8ef9db
JunRain <sa2313@tukorea.ac.kr>
2025-03-02 20:05:45 +0900

    [유전자] 풀이

--- 유전자.py
+++ 유전자.py
@@ -7,7 +7,5 @@ for i in range(1, n):
         if (s[j] == "a" and s[j + i] == "t") or (s[j] == "g" and s[j + i] == "c"):
             dp[j][j + i] = dp[j + 1][j + i - 1] + 2
         for k in range(j, j + i):
-            if dp[j][j + i] < dp[j][k] + dp[k + 1][j + i]:
-                dp[j][j + i] = dp[j][k] + dp[k + 1][j + i]
-
+            dp[j][j + i] = max(dp[j][j + i], dp[j][k] + dp[k + 1][j + i])
 print(dp[0][-1])
