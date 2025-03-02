bc3bfef00ee3a2b891f454810098538e216507cf
JunRain <sa2313@tukorea.ac.kr>
2025-03-02 18:40:26 +0900

    [멍멍이_쓰다듬기] 풀이

---
+++ 멍멍이_쓰다듬기.py
@@ -0,0 +1,13 @@
+def solve():
+    dif = b - a
+    day = 0
+    for i in range(1, 2 ** 31):
+        for _ in range(2):
+            if dif <= 0:
+                return day
+            day += 1
+            dif -= i
+
+
+a, b = map(int, input().split())
+print(solve())
