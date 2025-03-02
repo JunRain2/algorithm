86c477a341c35a77d6993022bc8af9fe4192b9e4
JunRain <sa2313@tukorea.ac.kr>
2025-03-02 19:54:39 +0900

    [유전자] 풀이

--- 유전자.py
+++ 유전자.py
@@ -1,31 +1,13 @@
-s = list(input())
-
-"""
-at와 gc는 가장 짧은 길이의 KOI 유전자
-어떤 X가 KOI 유전자라면, aXt와 gXc도 KOI 유전자
-어떤 X와 Y가 KOI 유전자라면 둘을 연결한 XY도 KOI 유전자
-"""
-
+s = input()
 n = len(s)
-stack = []
+dp = [[0] * n for _ in range(n)]
 
-result = 0
-for i in range(n):
-    if not stack:
-        stack.append(s[i])
-        continue
+for i in range(1, n):
+    for j in range(n - i):
+        if (s[j] == "a" and s[j + i] == "t") or (s[j] == "g" and s[j + i] == "c"):
+            dp[j][j + i] = dp[j + 1][j + i - 1] + 2
+        for k in range(j, j + i):
+            if dp[j][j + i] < dp[j][k] + dp[k + 1][j + i]:
+                dp[j][j + i] = dp[j][k] + dp[k + 1][j + i]
 
-    if "a" in stack and s[i] == "t":
-        result += 2
-        while stack[-1] != "a":
-            stack.pop()
-        stack.pop()
-    elif "g" in stack and s[i] == "c":
-        result += 2
-        while stack[-1] != "g":
-            stack.pop()
-        stack.pop()
-    else:
-        stack.append(s[i])
-
-print(result)
+print(dp[0][-1])
