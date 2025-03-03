cc9c40f5b6d1f2f3c97ef9ab30cfbf584ca8bedd
JunRain <sa2313@tukorea.ac.kr>
2025-03-03 17:50:36 +0900

    [SQL/LeetCode550] 풀이

--- SQL/LeetCode550.sql
+++ SQL/LeetCode550.sql
@@ -1,6 +1,9 @@
-SELECT ROUND(
-               COUNT(DISTINCT A.player_id)/(SELECT COUNT(DISTINCT player_id) FROM Activity)
-           , 2) AS fraction
-FROM Activity AS A, Activity AS B
-WHERE A.player_id = B.player_id
-  AND DATE_ADD(A.event_date, INTERVAL 1 DAY) = B.event_date;
+SELECT ROUND(COUNT(
+                     DISTINCT player_id)/ (SELECT COUNT(DISTINCT player_id)
+                                           FROM Activity), 2) AS fraction
+FROM Activity
+WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) IN
+      (
+          SELECT player_id , MIN(event_date) AS first_login
+          FROM Activity
+          GROUP BY player_id)
