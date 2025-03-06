82d6afb7888ea7b06fb35592b8dd17ade5eb81bd
JunRain <sa2313@tukorea.ac.kr>
2025-03-06 23:08:41 +0900

    [주유소] 풀이

--- 주유소.py
+++ 주유소.py
@@ -1,18 +1,13 @@
-# 도시의 개수
 n = int(input())
-load = list(map(int, input().split()))
+distances = list(map(int, input().split()))
 prices = list(map(int, input().split()))
 
-dist = load[n - 2]
-result = 0
-m = min(prices[: n - 1])
-for i in range(n - 2, 0, -1):
-    if prices[i] > m:
-        dist += load[i - 1]
-        continue
-    result += dist * m
-    m = min(prices[: n - i - 1])
-    dist = load[i - 1]
+min_price = prices[0]
+total_cost = 0
 
-result += prices[0] * dist
-print(result)
+for i in range(n-1):
+    if prices[i] < min_price:
+        min_price = prices[i]
+    total_cost += min_price * distances[i]
+
+print(total_cost)
