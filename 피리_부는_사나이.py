d2121be9e071ffd7b4425a2616c9a846665ef84b
JunRain <sa2313@tukorea.ac.kr>
2025-03-07 20:14:21 +0900

    [피리_부는_사나이] 내 풀이

--- 피리_부는_사나이.py
+++ 피리_부는_사나이.py
@@ -1,34 +1,29 @@
-import sys
-input = sys.stdin.readline
+n, m = map(int, input().split())
+graph = [list(input()) for _ in range(n)]
+visited = [[0] * m for _ in range(n + 1)]
+
+dir = dict()
+dir["D"] = (1, 0)
+dir["L"] = (0, -1)
+dir["R"] = (0, 1)
+dir["U"] = (-1, 0)
 
 def dfs(x, y):
-    if visited[x][y]:
-        if visited[x][y] == 1:  # 현재 탐색 중인 경로에서 사이클 발견
-            return 1
-        return 0  # 이미 완료된 경로
+    if x < 0 or x >= n or y < 0 or y >= m or visited[x][y] == 2:
+        return 0
+    if visited[x][y] == 1:
+        return 1
     
-    visited[x][y] = 1  # 현재 탐색 중 표시
-    nx, ny = x + direction[graph[x][y]][0], y + direction[graph[x][y]][1]
+    visited[x][y] = 1
+    nx, ny = x + dir[graph[x][y]][0], y + dir[graph[x][y]][1]
     result = dfs(nx, ny)
-    visited[x][y] = 2  # 탐색 완료 표시
+    visited[x][y] = 2
     return result
 
-# 입력 받기
-N, M = map(int, input().split())
-graph = [list(input().strip()) for _ in range(N)]
-
-# 방향 정의
-direction = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
-
-# 방문 배열 초기화
-visited = [[0] * M for _ in range(N)]
-
-safe_zones = 0
-
-# 모든 칸에 대해 DFS 수행
-for i in range(N):
-    for j in range(M):
+result = 0
+for i in range(n):
+    for j in range(m):
         if not visited[i][j]:
-            safe_zones += dfs(i, j)
-
-print(safe_zones)
+            result += dfs(i, j)
+            
+print(result)
