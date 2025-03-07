1cdce6bfae4bb905419a1b7fd5d3a472acbbe76a
JunRain <sa2313@tukorea.ac.kr>
2025-03-07 17:24:26 +0900

    [유전자] 내 풀이

--- 유전자.py
+++ 유전자.py
@@ -1,11 +1,15 @@
-s = input()
+s = list(input())
 n = len(s)
-dp = [[0] * n for _ in range(n)]
 
-for i in range(1, n):
-    for j in range(n - i):
-        if (s[j] == "a" and s[j + i] == "t") or (s[j] == "g" and s[j + i] == "c"):
-            dp[j][j + i] = dp[j + 1][j + i - 1] + 2
-        for k in range(j, j + i):
-            dp[j][j + i] = max(dp[j][j + i], dp[j][k] + dp[k + 1][j + i])
-print(dp[0][-1])
+dp = [[0] * (n + 1) for _ in range(n + 1)]
+
+for i in range(n, 0, -1):
+    for j in range(i + 1, n + 1):
+        if (s[i - 1] == "a" and s[j - 1] == "t") or (s[i - 1] == "g" and s[j - 1] == "c"):
+            dp[i][j] = dp[i + 1][j - 1] + 2
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j-1])
+        for k in range(i, j + 1):
+            dp[i][j] = max(dp[i][j], dp[i][k] + dp[k][j])
+
+print(max(map(max, dp)))
