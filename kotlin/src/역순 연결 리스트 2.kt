e66e5e05793d329bb68714430c6fe2dae7fe5ef7
JunRain <sa2313@tukorea.ac.kr>
2025-08-20 23:05:04 +0900

    역순 연결 리스트 2

---
+++ kotlin/src/역순 연결 리스트 2.kt
@@ -0,0 +1,27 @@
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+class Solution {
+    fun reverseBetween(head: ListNode?, left: Int, right: Int): ListNode? {
+        if (head == null) {
+            return null
+        }
+        val root = ListNode(0)
+        root.next= head
+
+        var start = root
+        for (i in 0..<left) {
+            start = start.next
+        }
+        val end = start.next
+        for (i in 0..<(right - left)) {
+            val tmp = start.next
+            start.next = end.next
+            end.next = end.next.next
+            start.next.next = tmp
+        }
+
+        return root.next
+    }
+}
